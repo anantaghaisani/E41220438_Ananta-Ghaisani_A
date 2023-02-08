6d2e032bb6b30686f55d7269d5387738d10dce50
anantaghaisani <124554698+anantaghaisani@users.noreply.github.com>
2023-02-08 08:56:20 +0700

    Create GitHub.java

---
+++ GitHub.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author VivoBook
+ */
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 2; while (i <= 20) { 
+        System.out.println("Hello Ananta Cantik!");
+        i++;
+    }
+    
+}}
